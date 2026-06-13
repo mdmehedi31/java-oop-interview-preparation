fd7ccd4aeb1c3fbe4e21d4e347cbe28c97d8be4b
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-06-13 22:04:50 +0600

    final property

--- src/com/interview/preparation/encapsulation/Person.java
+++ src/com/interview/preparation/encapsulation/Person.java
@@ -1,8 +1,38 @@
 package com.interview.preparation.encapsulation;
 
-public final class  Person {
+public  class  Person {
+
+
+    private final int age;
+    private int number;
 
     public void showPersonDetails() {
         System.out.println("Person Details");
+    }
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    /*
+    This is not possible to set the value by using
+    the method cause its final, we only initialize it once time.
+
+
+    public void setAge(int age) {
+        this.age=age;
+    }
+    */
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
     }
 }
